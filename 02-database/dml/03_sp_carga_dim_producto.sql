acdadf501a2cd1b037aa4d930337439f177ac084
Fran Veliz <velizyexon@gmail.com>
2025-12-19 00:17:50 -0500

    Refactor Dim_Producto load to incremental using MERGE (SCD Type 1)

--- 02-database/dml/03_sp_carga_dim_producto.sql
+++ 02-database/dml/03_sp_carga_dim_producto.sql
@@ -1,36 +1,51 @@
 USE AventureWorks_DWH;
 GO 
-
-CREATE OR ALTER PROCEDURE DW.sp_Carga_DimProducto 
+CREATE OR ALTER PROCEDURE DW.sp_Carga_DimProducto
 AS
 BEGIN
- SET NOCOUNT ON ;
- TRUNCATE TABLE DW.Dim_Producto;
- 
- INSERT INTO  DW.Dim_producto(
-  ProductID_Source,
-  NombreProducto,
-  Color, 
-  Subcategoria, 
-  Categoria
- )
- SELECT
-  P.ProductID,
-  P.Name,
-  ISNULL( P.Color,'N/A'),
-  ISNULL(S.name,'Sin SubCategoria'),
-  ISNULL( C.Name,'Sin Categoria')
+    SET NOCOUNT ON;
 
- FROM 
- AdventureWorks2019.Production.Product P 
- LEFT JOIN AdventureWorks2019.Production.ProductSubcategory S
-      ON P.ProductSubcategoryID = S.ProductSubcategoryID
- LEFT JOIN AdventureWorks2019.Production.ProductCategory C
-      ON S.ProductCategoryID=C.ProductCategoryID;
+    MERGE DW.Dim_Producto AS target
+    USING (
+        SELECT
+            P.ProductID,
+            P.Name AS NombreProducto,
+            ISNULL(P.Color, 'N/A') AS Color,
+            ISNULL(S.Name, 'Sin SubCategoria') AS Subcategoria,
+            ISNULL(C.Name, 'Sin Categoria') AS Categoria
+        FROM AdventureWorks2019.Production.Product P
+        LEFT JOIN AdventureWorks2019.Production.ProductSubcategory S
+            ON P.ProductSubcategoryID = S.ProductSubcategoryID
+        LEFT JOIN AdventureWorks2019.Production.ProductCategory C
+            ON S.ProductCategoryID = C.ProductCategoryID
+    ) AS source
+    ON target.ProductID_Source = source.ProductID
 
-PRINT('LOG : Dim_Producto cargada exitosamente ')
+    WHEN MATCHED THEN
+        UPDATE SET
+            NombreProducto = source.NombreProducto,
+            Color = source.Color,
+            Subcategoria = source.Subcategoria,
+            Categoria = source.Categoria,
+            FechaCarga = GETDATE(),
+            UsuarioCarga = SYSTEM_USER
+
+    WHEN NOT MATCHED THEN
+        INSERT (
+            ProductID_Source,
+            NombreProducto,
+            Color,
+            Subcategoria,
+            Categoria
+        )
+        VALUES (
+            source.ProductID,
+            source.NombreProducto,
+            source.Color,
+            source.Subcategoria,
+            source.Categoria
+        );
+
+    PRINT('LOG : Dim_Producto cargada incrementalmente');
 END;
 GO
-
---EXEC DW.sp_Carga_DimProducto;
---SELECT TOP 10 FROM DW.Dim_Producto;"""
